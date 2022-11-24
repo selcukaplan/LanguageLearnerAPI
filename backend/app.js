bde73bb2216ef07a94f59d86ffeb13a1132c90ef
deneme <deneme@hotmail.com>
2022-11-24 18:43:11 +0300

    start web backend funct added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 
 
-
-
 const ExpressServer = require('./expressServer.js');
+
+const MongoDB= require("./database/mongoDB");
 
 const expressServer=ExpressServer.createServerFromConfig();
 
-expressServer.start();
+async function startWebBackend() {
+    try {
+        await MongoDB.connect();
+        expressServer.start();
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1);
+    }
+
+}
+
+startWebBackend();
