4ba62fd5a30dbee07b37150ae827a5c0c32f366e
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-11-26 23:16:54 +0300

    user controller created

---
+++ backend/controllers/userController.js
@@ -0,0 +1,27 @@
+
+const {StatusCodes} = require('http-status-codes');
+const User = require('../models/user');
+const ResponseController = require('./responseController');
+const jwt=require('jsonwebtoken');
+
+class UserController {
+
+    static #user=new User();
+
+     static async #createToken(userInfo) {
+         return await jwt.sign({userInfo}, process.env.JWT_SECRET,
+             {expiresIn: process.env.JWT_LIFETIME}
+         );
+
+     }
+
+    static async signUpUser(request,response) {
+        const newUser = await UserController.#user.signUp(request.body);
+        const newUserToken = await UserController.#createToken(newUser._id);
+        const responseData=ResponseController.getDataResponse(newUserToken)
+        response.status(StatusCodes.CREATED).json(responseData);
+    }
+
+}
+
+module.exports = UserController;
