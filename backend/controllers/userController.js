5b1800643d8995560eb3b1a10231e4059e2243a1
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-11-29 22:47:50 +0300

    login, get friends and  get users having the same foreign languages methods have been added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const {StatusCodes} = require('http-status-codes');
 const User = require('../models/user');
 const ResponseController = require('./responseController');
 const jwt=require('jsonwebtoken');
+const {UnAuthenticated} = require("../errors");
 
 class UserController {
 
@@ -15,12 +16,70 @@ class UserController {
 
      }
 
+     static #getUserId(request) {
+         const userId=request.user;
+         if (!userId) {
+             throw new UnAuthenticated('Authentication was not provided!');
+         }
+         return userId;
+     }
+
     static async signUpUser(request,response,next) {
          try {
              const newUser = await UserController.#user.signUp(request.body);
-             const newUserToken = await UserController.#createToken(newUser._id);
+             const newUserToken = await UserController.#createToken(newUser.id);
              const responseData = ResponseController.getDataResponse(newUserToken)
              response.status(StatusCodes.CREATED).json(responseData);
+         } catch (error) {
+             next(error);
+         }
+    }
+
+    static async loginUser(request,response,next) {
+         try {
+             const user=await UserController.#user.login(request.body);
+             const userToken=await UserController.#createToken(user.id);
+             const responseData=ResponseController.getDataResponse(userToken);
+             response.status(StatusCodes.OK).json(responseData);
+         } catch (error) {
+             next(error);
+         }
+
+    }
+
+    static async getUsersWithSameForeignLanguages(request,response,next) {
+         try {
+                const currentUserId = UserController.#getUserId(request);
+                // Todo: foreign languages will be stored as a map in the database
+                const currentUser=await UserController.#user.getModel()
+                    .findById(currentUserId);
+                const currentUserForeignLanguages = currentUser.foreignLanguages;
+                const otherUsers=await UserController.#user.getModel()
+                    .find({'foreignLanguages' : { $in : currentUserForeignLanguages}, "_id" : { $ne : currentUserId}});
+                const responseData = ResponseController.getDataResponse(otherUsers);
+                response.status(StatusCodes.OK).json(responseData);
+         } catch (error) {
+             next(error);
+         }
+    }
+
+    static async getFriendsOfUser(request,response,next) {
+        try {
+            const currentUserId = UserController.#getUserId(request);
+            const friendsOfUser=await UserController.#user.getModel()
+                .findById(currentUserId).select('friends');
+            const responseData = ResponseController.getDataResponse(friendsOfUser);
+            response.status(StatusCodes.OK).json(responseData);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async getUsers(request,response,next) {
+         try {
+             const users = await UserController.#user.getModel().find();
+             const responseData = ResponseController.getDataResponse(users);
+             response.status(StatusCodes.OK).json(responseData);
          } catch (error) {
              next(error);
          }
