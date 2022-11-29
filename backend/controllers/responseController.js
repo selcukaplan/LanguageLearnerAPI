97c1726c8bb7430bb9fe09e519b14bd568e0bf87
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-11-29 10:30:45 +0300

    error status property was removed from error response object because it is unnecessary.

--- backend/controllers/responseController.js
+++ backend/controllers/responseController.js
@@ -1,12 +1,10 @@
 
-const {StatusCodes} = require('http-status-codes');
 
 class ResponseController {
 
     static getErrorResponse(error) {
         return {
             isDone: false,
-            errorStatus: error.status || StatusCodes.INTERNAL_SERVER_ERROR,
             error : error.message || error
         }
 
