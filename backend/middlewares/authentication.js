a0cfb7e2a8d1013b835313ba69c67064623d9774
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-11-30 23:09:49 +0300

    jwt token error checker added

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -16,14 +16,17 @@ async function  authenticateTheUser(request,response,next) {
     try {
         const {authorization} = request.headers;
         if (!isHeaderValid(authorization)) {
-            throw new UnAuthenticated('Authentication is invalid!');
+            throw new UnAuthenticated('Authentication Header is invalid!');
         }
         const encryptedToken = authorization.split(' ')[1];
-        //TODO: jwt.verify error check will be added
         const userToken = await jwt.verify(encryptedToken, process.env.JWT_SECRET);
-        request.user = userToken.userInfo;
+        request.user = userToken.userInfo; // Todo: does 'userInfo' exist or not,checker could be added
         next();
     } catch (error) {
+        const errorClassName=error.constructor.name;
+        if (errorClassName === 'JsonWebTokenError') {
+            error=new UnAuthenticated('Token is invalid!');
+        }
         next(error);
     }
 
