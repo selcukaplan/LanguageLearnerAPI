24425c2926fe234dceb8e4697dd50bcf28566e94
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-12-02 18:10:59 +0300

    update comment method and authentication checker were added to the route

--- backend/routes/commentRouter.js
+++ backend/routes/commentRouter.js
@@ -9,7 +9,9 @@ const commentRouter = express.Router();
 
 commentRouter.post('/:receiverId', [authenticateTheUser,CommentController.createComment]);
 
-commentRouter.get('/:userId', [CommentController.getCommentsOfUser]);
+commentRouter.get('/:userId', [authenticateTheUser, CommentController.getCommentsOfUser]);
+
+commentRouter.patch('/:commentId', [authenticateTheUser, CommentController.updateComment]);
 
 commentRouter.delete('/:commentId',[authenticateTheUser,CommentController.removeComment]);
 
