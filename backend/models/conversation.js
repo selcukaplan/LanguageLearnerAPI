4fd15ddd1d6654406a4ddbfb63172fe2ebbf0211
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-12-04 22:50:04 +0300

    validator added for conversation model

--- backend/models/conversation.js
+++ backend/models/conversation.js
@@ -1,5 +1,5 @@
 
-const MongoDBCollection=require('mongoDBCollection');
+const MongoDBCollection=require('./mongoDBCollection');
 
 class Conversation extends  MongoDBCollection{
 
@@ -10,9 +10,17 @@ class Conversation extends  MongoDBCollection{
             // TODO: Index property can be added to decrease complexity
             //TODO: hash set can be used because members are unique
             type:Array,
-            required:true
+            required: true,
+            validate:  [Conversation.#hasTwoMembers,"At least two members must exist"]
         }
     }
+
+    static #hasTwoMembers(arr) { // Todo: could be generic method for all mongoDB collections
+        return arr.length >= 2;
+
+    }
+
+
     constructor(collectionName=Conversation.#conversationName,
                 collectionDefinitions = Conversation.#conversationDefinitions) {
         super(collectionName,collectionDefinitions);
