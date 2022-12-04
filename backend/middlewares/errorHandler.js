cfcd7a3cbe89e608853c936ecca23f31b86b1da0
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-12-04 22:44:26 +0300

    response controller methods updated

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -5,7 +5,7 @@ const ResponseController = require('../controllers/responseController');
 
 async function errorHandlerMiddleware (error,request,response,next) {
     let statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
-    return response.status(statusCode).json(ResponseController.getErrorResponse(error));
+    return response.status(statusCode).json(ResponseController.createResponseError(error));
 
 }
 
