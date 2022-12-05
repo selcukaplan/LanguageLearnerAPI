819998e96cb859a0af2704054c9465bb4749d2ad
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-12-05 23:43:05 +0300

    some routes don't need to authenticate the user. It could be removed

--- backend/routes/conversationRouter.js
+++ backend/routes/conversationRouter.js
@@ -7,6 +7,8 @@ const authenticateTheUser = require('../middlewares/authentication');
 
 const commentRouter = express.Router();
 
+//TODO: some routes don't need to authenticate the user. It could be removed
+
 commentRouter.post('/', [authenticateTheUser,ConversationController.createConversation]);
 
 commentRouter.get('/', [authenticateTheUser, ConversationController.getConversationOfMembers]);
