2f262b3ff91ab88f4185439b8ac7683010cc2b78
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-12-16 16:38:09 +0300

    Todo comments added to remember the implementation of hash set or hash map

--- backend/models/conversation.js
+++ backend/models/conversation.js
@@ -8,8 +8,7 @@ class Conversation extends  MongoDBCollection{
 
     static #conversationDefinitions = {
         members: { //TODO: ref will be added for population
-            // TODO: Index property can be added to decrease complexity
-            //TODO: hash set can be used because members are unique
+            //TODO: map could be used to store unique members
             type:Array,
             required: true,
             validate:  [Conversation.#hasTwoMembers,"At least two members must exist"]
@@ -26,17 +25,18 @@ class Conversation extends  MongoDBCollection{
         super(collectionName,collectionDefinitions);
     }
 
-    async addMembersToConversation(conversationId,newMembers) {
+    async addMembersToConversation(conversationId,newMembersList) {
+        //Todo: check members exist, before pushing them
        const updatedConversation =await this.getModel()
-           .findByIdAndUpdate(conversationId,{$push : {"members": {$each : newMembers}}});
+           .findByIdAndUpdate(conversationId,{$push : {"members": {$each : newMembersList}}});
         if (!updatedConversation) {throw new BadRequest('conversation could not be updated!')}
         return updatedConversation;
 
     }
 
-    async removeMembersFromConversation(conversationId,removedMembers) {
+    async removeMembersFromConversation(conversationId,removedMembersList) {
         await this.getModel()
-            .findByIdAndUpdate(conversationId,{ $pull : {"members" :  {$in : removedMembers}}});
+            .findByIdAndUpdate(conversationId,{ $pull : {"members" :  {$in : removedMembersList}}});
 
     }
 
