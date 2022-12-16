cfd4dbdf5ff2e97082a751edc46ae020ca85ac36
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-12-16 15:05:10 +0300

    authentication for the http middleware path updated

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,7 +3,7 @@ const express=require('express');
 
 const UserController=require('../controllers/userController');
 
-const authenticateTheUser = require('../middlewares/authentication');
+const {authenticateTheUserForHTTP} = require('../middlewares/authentication');
 
 const userRouter = express.Router();
 
@@ -11,16 +11,16 @@ userRouter.post('/signup', UserController.signUpUser);
 
 userRouter.post('/login', UserController.loginUser);
 
-userRouter.get('/foreignLanguages', [authenticateTheUser,UserController.getUsersHasSameForeignLanguages]);
+userRouter.get('/foreignLanguages', [authenticateTheUserForHTTP,UserController.getUsersHasSameForeignLanguages]);
 
-userRouter.get('/friends',[authenticateTheUser,UserController.getFriendsOfUser]);
+userRouter.get('/friends',[authenticateTheUserForHTTP,UserController.getFriendsOfUser]);
 
 userRouter.get('/', [UserController.getUsers]);
 
-userRouter.patch('/update',[authenticateTheUser,UserController.updateUser]);
+userRouter.patch('/update',[authenticateTheUserForHTTP,UserController.updateUser]);
 
-userRouter.patch('/addFriend/:friendId',[authenticateTheUser,UserController.addFriendToUser]);
+userRouter.patch('/addFriend/:friendId',[authenticateTheUserForHTTP,UserController.addFriendToUser]);
 
-userRouter.patch('/removeFriend/:friendId',[authenticateTheUser,UserController.removeFriendFromUser]);
+userRouter.patch('/removeFriend/:friendId',[authenticateTheUserForHTTP,UserController.removeFriendFromUser]);
 
 module.exports=userRouter;
