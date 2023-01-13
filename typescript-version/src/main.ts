5ad1cff3122a84a41cfc3a92f503189cbd8f8259
KaplanCoder <kemalselcukkaplan@gmail.com>
2023-01-13 20:22:05 +0300

    start web backend method added

--- typescript-version/src/main.ts
+++ typescript-version/src/main.ts
@@ -1,10 +1,23 @@
 
-import 'dotenv/config';
+
+
+
+import  ExpressServer from "./servers/expressServer";
 
 import MongoDB from "./database/mongoDB";
 
-async function start() {
-    console.log(await MongoDB.connect(process.env.MONGO_URI));
+
+async function startWebBackend(): Promise<any> {
+    try {
+        const expressServer: ExpressServer = ExpressServer.createServerFromConfig();
+        console.log(await MongoDB.connect());
+        expressServer.start();
+    } catch (error) {
+        let errorMessage: string = (error as Error).message;
+        console.log(errorMessage);
+        process.exit(1);
+    }
+
 }
 
-start();
+startWebBackend();
