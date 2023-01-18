0a90a13686fbc86ffcea5d0c04d0b3a77499a63e
KaplanCoder <kemalselcukkaplan@gmail.com>
2023-01-18 15:49:24 +0300

    mongo db collection with typescript created

---
+++ typescript-version/src/models/mongoDBcollection.ts
@@ -0,0 +1,79 @@
+
+import mongoose, {Schema,Model} from "mongoose"
+
+import assert from 'assert'
+
+export default abstract class MongoDBCollection<DocumentInterface> {
+
+    private collectionName: string;
+
+    private collectionDefinitions: object;
+
+    private collectionOptions: object;
+
+    private collectionSchema: Schema;
+
+    private collectionModel: Model<DocumentInterface>;
+
+    constructor(collectionName: string,collectionDefinitions: object,collectionOptions = {timestamps: true}) {
+        this.collectionDefinitions=collectionDefinitions;
+        this.collectionOptions=collectionOptions;
+        this.collectionName=collectionName;
+        this.collectionSchema=this.createSchema();
+        this.collectionModel=this.createModel();
+    }
+
+    private createSchema() {
+        return new Schema<DocumentInterface>(this.collectionDefinitions,this.collectionOptions);
+    }
+
+    private createModel() {
+        assert(this.collectionSchema," model can not be created if  schema is undefined!");
+        // If model exists, It does not create the same model again  ---> to prevent that error
+        const model:Model<DocumentInterface> = mongoose.models[this.collectionName] || mongoose.model(this.collectionName,this.collectionSchema)
+        return model;
+    }
+
+    /*
+        isSubSetOfDefinitions(definitionsObject) {
+            if (Object.keys(definitionsObject).length === 0) {return false};
+            for (let currentDefinition in definitionsObject) {
+                if (!this.#collectionDefinitions.hasOwnProperty(currentDefinition)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+
+        getRequiredDefinitionKeys() {
+            const requiredDefinitionKeys=[];
+            for (let currentDefinition in this.#collectionDefinitions) {
+                let currentDefinitionObject=this.#collectionDefinitions[currentDefinition];
+                if (currentDefinitionObject.hasOwnProperty("required")
+                    && currentDefinitionObject["required"] !== false) {
+                    requiredDefinitionKeys.push(currentDefinition);
+                }
+            }
+            return requiredDefinitionKeys;
+        }
+
+
+        areRequiredDefinitionKeysMatched(definitionsObject) {
+            let requiredDefinitionKeys= this.getRequiredDefinitionKeys().sort(); // TODO: sort algorithm could be improved
+            const objectKeys=Object.keys(definitionsObject).sort();// TODO: sort algorithm could be improved
+            return JSON.stringify(requiredDefinitionKeys) === JSON.stringify(objectKeys); // Keys are compared
+        }
+
+
+     */
+
+
+    getModel() : Model<DocumentInterface> {
+        return this.collectionModel;
+    }
+
+
+
+}
