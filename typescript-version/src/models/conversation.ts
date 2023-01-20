376e45ddebd04aa62a1fe3f29ccc07a6ac020f2e
KaplanCoder <kemalselcukkaplan@gmail.com>
2023-01-20 15:20:54 +0300

    conversation model created

---
+++ typescript-version/src/models/conversation.ts
@@ -0,0 +1,75 @@
+import MongoDBCollection from "./mongoDBcollection";
+
+
+interface IConversation {
+    members: Array<number>;
+    createdAt:Date;
+    updatedAt:Date;
+    
+}
+
+class Conversation extends  MongoDBCollection<IConversation>{
+
+    private static conversationName="Conversation";
+
+    //Todo: when conversation is removed, all messages belong to it must also  be removed.
+
+    private static conversationDefinitions = {
+        members: { //TODO: ref will be added for population
+            //TODO: map could be used to store unique members
+            type:Array,
+            required: true,
+            validate:  [Conversation.hasTwoMembers,"At least two members must exist"]
+        }
+    }
+
+    private static hasTwoMembers (arr: Array<number>) : boolean {
+        return arr.length >= 2;
+
+    }
+
+    constructor(conversationName=Conversation.conversationName,
+                conversationDefinitions = Conversation.conversationDefinitions) {
+        super(conversationName,conversationDefinitions);
+    }
+
+    async createConversation(members: Array<number>) {
+        const newConversation : (IConversation | null)  =await this.getModel().create({members});
+        if (!newConversation) {
+            throw new Error('Conversation is not created');
+        }
+        return newConversation;
+    }
+
+    async getConversation(members: Array<number>): Promise<IConversation> {
+        const conversation : (IConversation | null) = await this.getModel().findOne({"members" : {$all : members}});
+        // assertion => only one conversation should be found!
+        if (!conversation) {
+            throw new Error('conversation  is not found!');
+        }
+        return conversation;
+
+    }
+
+    async addMembersToConversation(conversationId: number, newMembersList: Array<number>) : Promise<IConversation>{
+        //Todo: check members exist, before pushing them
+       const updatedConversation :  (IConversation | null) =await this.getModel()
+           .findByIdAndUpdate(conversationId,{$push : {"members": {$each : newMembersList}}});
+        if (!updatedConversation) {throw new Error('conversation could not be updated!')}
+        return updatedConversation;
+
+    }
+
+    async removeMembersFromConversation(conversationId: number,removedMembersList: Array<number>) : Promise<IConversation> {
+        const updatedConversation : (IConversation | null) = await this.getModel()
+            .findByIdAndUpdate(conversationId,{ $pull : {"members" :  {$in : removedMembersList}}});
+        if (!updatedConversation) {
+            throw new Error('no members are removed from the conversation');
+        }
+        return updatedConversation;
+    }
+
+
+}
+
+module.exports = Conversation;
