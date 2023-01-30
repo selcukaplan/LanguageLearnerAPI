0d3cff578b693c9280467303b9bb91afe4c6eae0
KaplanCoder <kemalselcukkaplan@gmail.com>
2023-01-30 19:23:24 +0300

    middlewares are binded to express server and its path

--- typescript-version/src/main.ts
+++ typescript-version/src/main.ts
@@ -4,20 +4,27 @@
 
 import  ExpressServer from "./servers/expressServer";
 
+import errorHandlerMiddleware from "./middlewares/errorHandler";
+
 import MongoDB from "./database/mongoDB";
 
+import userRouter from "./routes/userRouter";
 
-async function startWebBackend(): Promise<any> {
+const expressServer: ExpressServer = ExpressServer.createServerFromConfig();
+
+expressServer.bindRequestHandlerToPath('/api/v1/users',userRouter);
+
+expressServer.bindRequestHandler(errorHandlerMiddleware);
+
+async function startWebBackend() {
     try {
-        const expressServer: ExpressServer = ExpressServer.createServerFromConfig();
         console.log(await MongoDB.connect());
-        expressServer.start();
+        console.log(await expressServer.start());
     } catch (error) {
         let errorMessage: string = (error as Error).message;
         console.log(errorMessage);
         process.exit(1);
     }
-
 }
 
 startWebBackend();
