5352ec43ab133242409233d7712106a7d2a5b0b7
KaplanCoder <kemalselcukkaplan@gmail.com>
2023-01-30 19:20:47 +0300

    access modifier changed

--- typescript-version/src/errors/httpErrors/HTTPError.ts
+++ typescript-version/src/errors/httpErrors/HTTPError.ts
@@ -3,7 +3,7 @@
 
 export class HTTPError extends Error {
 
-    private statusCode: number;
+    statusCode: number;
 
     constructor(message: string,statusCode: number) {
         super(message);
